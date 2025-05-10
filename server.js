175224538ac80b3dfd270933a85a8347dda19904
lupe181001 <sauloduarte45@gmail.com>
2025-05-10 18:55:27 -0300

    Envio completo do projeto

---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const porta = 3000
+app.use(express.static('public'))
+
+app.get('/filme', async (req, res) => {
+    const {titulo} = req.query
+
+    if(!titulo) {
+        return res.status(400).json({error: "Por favor, informe título do filme"})
+    }
+    try {
+        const resposta = await axios.get('http://www.omdbapi.com/', {
+            params: {
+                t: titulo,
+                apiKey: process.env.OMDB_API_KEY
+            }
+        })
+
+        const filme = resposta.data
+
+        if (filme.Response === "False") {
+            return res.status(404).json({error: "Filme não encontrado"})
+        }
+        res.json ({
+            titulo: filme.Title,
+            ano: filme.Year,
+            diretor: filme.Director,
+            genero: filme.Genre,
+            enredo: filme.Plot,
+            poster: filme.Poster,
+            duracao: filme.Runtime
+        })
+    } catch (error) {
+        res.status(500).json({error: "Erro ao buscar filme"})
+    }
+})
+
+app.listen(porta, () => {
+    console.log("Servidor rodando na porta 3000")
+})
